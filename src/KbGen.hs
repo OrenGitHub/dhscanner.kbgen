f1b8c4cbec079f6a656054d811617b458c993268
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-04-21 20:02:49 +0300

    Fix ( #1 ) call locations

--- src/KbGen.hs
+++ src/KbGen.hs
@@ -10,10 +10,17 @@ where
 
 -- project imports
 import Fqn
+import Cfg
 import Callable
 import Location
 
+-- project qualified imports
+import qualified Bitcode
+
 -- general imports
+import Data.Set
+import Data.List
+import Data.Maybe
 import Data.Aeson
 import GHC.Generics
 
@@ -47,11 +54,23 @@ emptyKnowledgeBase = KnowledgeBase [] []
 
 -- | API: generate a prolog knowledge base from
 -- a collection of callables
-kbGen :: Callable -> KnowledgeBase
-kbGen (Callable.Script script) = kbGenScript script
-kbGen (Callable.Lambda lambda) = emptyKnowledgeBase -- kbGenLambda lambda
-kbGen (Callable.Method method) = emptyKnowledgeBase
-kbGen (Callable.Function func) = emptyKnowledgeBase
+kbGen :: Callables -> KnowledgeBase
+kbGen = kbGen' . actualCallables
+
+kbGen' :: [ Callable ] -> KnowledgeBase
+kbGen' [] = emptyKnowledgeBase
+kbGen' (c:cs) = let
+    kb = kbGen'' c
+    rest = kbGen' cs
+    calls' = (calls kb) ++ (calls rest)
+    args' = (args kb) ++ (args rest)
+    in KnowledgeBase { calls = calls', args = args' }
+
+kbGen'' :: Callable -> KnowledgeBase
+kbGen'' (Callable.Script script) = kbGenScript script
+kbGen'' (Callable.Lambda lambda) = emptyKnowledgeBase -- kbGenLambda lambda
+kbGen'' (Callable.Method method) = emptyKnowledgeBase
+kbGen'' (Callable.Function func) = emptyKnowledgeBase
 
 kbGenScript :: Callable.ScriptContent -> KnowledgeBase
 kbGenScript script = let
@@ -59,8 +78,24 @@ kbGenScript script = let
     args' = kbGenScriptArgs script
     in KnowledgeBase calls args'
 
+keepCalls :: Bitcode.InstructionContent -> Maybe Bitcode.CallContent
+keepCalls (Bitcode.Call call) = Just call 
+keepCalls _ = Nothing
+
+combine :: [ Fqn ] -> [ Location ] -> [ Call ]
+combine [] _ = []
+combine _ [] = []
+combine (fqn:fqns) (loc:locs) = (Call fqn loc) : (combine fqns locs)
+
 kbGenScriptCalls :: Callable.ScriptContent -> [ Call ]
-kbGenScriptCalls script = []
+kbGenScriptCalls script = let
+    nodes = Cfg.actualNodes $ Cfg.nodes (scriptBody script)
+    instructions = Data.Set.map Bitcode.instructionContent (Data.Set.map Cfg.theInstructionInside nodes)
+    calls = catMaybes (Data.Set.toList (Data.Set.map keepCalls instructions))
+    callees = Data.List.map Bitcode.callee calls
+    fqns = Data.List.map Bitcode.variableFqn callees
+    locations = Data.List.map Bitcode.callLocation calls
+    in combine fqns locations
 
 kbGenScriptArgs :: Callable.ScriptContent -> [ Arg ]
 kbGenScriptArgs script = []
