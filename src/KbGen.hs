7f142e03fe476c3a640e35f7e1a84d82898cef25
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-05-22 09:54:18 +0300

    handle methods

--- src/KbGen.hs
+++ src/KbGen.hs
@@ -35,7 +35,8 @@ data KnowledgeBase
           args :: [ Arg ],
           lambdas :: [ KBLambda ],
           params :: [ Param ],
-          dataflow :: [ Edge ]
+          dataflow :: [ Edge ],
+          funcs :: [ KBFunc ]
      }
      deriving ( Show, Generic, ToJSON )
 
@@ -72,6 +73,14 @@ data KBLambda
      }
      deriving ( Show, Generic, ToJSON )
 
+data KBFunc
+   = KBFunc
+     {
+         funcLocation :: Location
+     }
+     deriving ( Show, Generic, ToJSON )
+
+
 data Param
    = Param
      {
@@ -82,7 +91,7 @@ data Param
      deriving ( Show, Generic, ToJSON )
 
 emptyKnowledgeBase :: KnowledgeBase
-emptyKnowledgeBase = KnowledgeBase [] [] [] [] []
+emptyKnowledgeBase = KnowledgeBase [] [] [] [] [] []
 
 -- | API: generate a prolog knowledge base from
 -- a collection of callables
@@ -99,13 +108,23 @@ kbGen' (c:cs) = let
     lambdas' = (lambdas kb) ++ (lambdas rest)
     params' = (params kb) ++ (params rest)
     dataflow' = (dataflow kb) ++ (dataflow rest)
-    in KnowledgeBase calls' args' lambdas' params' dataflow' 
+    funcs' = (funcs kb) ++ (funcs rest)
+    in KnowledgeBase calls' args' lambdas' params' dataflow' funcs' 
 
 kbGen'' :: Callable -> KnowledgeBase
 kbGen'' (Callable.Script script) = kbGenScript script
 kbGen'' (Callable.Lambda lambda) = kbGenLambda lambda
-kbGen'' (Callable.Method method) = emptyKnowledgeBase
+kbGen'' (Callable.Method method) = kbGenMethod method
 kbGen'' (Callable.Function func) = kbGenFunction func
+
+kbGenMethod:: Callable.MethodContent -> KnowledgeBase
+kbGenMethod method = let
+    calls' = kbGenCalls (Callable.methodBody method)
+    args' = kbGenArgs (Callable.methodBody method)
+    params' = extractMethodParams method
+    dataflow' = extractMethodDataflow method
+    funcs' = [ KBFunc (Callable.methodLocation method) ]
+    in KnowledgeBase calls' args' [] params' dataflow' funcs' 
 
 kbGenFunction :: Callable.FunctionContent -> KnowledgeBase
 kbGenFunction func = let
@@ -113,13 +132,14 @@ kbGenFunction func = let
     args' = kbGenArgs (Callable.funcBody func)
     params' = extractFunctionParams func
     dataflow' = extractFunctionDataflow func
-    in KnowledgeBase calls' args' [] params' dataflow' 
+    funcs' = [ KBFunc (Callable.funcLocation func) ]
+    in KnowledgeBase calls' args' [] params' dataflow' funcs' 
 
 kbGenScript :: Callable.ScriptContent -> KnowledgeBase
 kbGenScript script = let
     calls' = kbGenScriptCalls script
     args' = kbGenScriptArgs script
-    in KnowledgeBase calls' args' [] [] []
+    in KnowledgeBase calls' args' [] [] [] []
 
 kbGenLambda :: Callable.LambdaContent -> KnowledgeBase
 kbGenLambda lambda = let
@@ -128,10 +148,13 @@ kbGenLambda lambda = let
     lambdas' = [ KBLambda (Callable.lambdaLocation lambda) ]
     params' = extractLambdaParams lambda
     dataflow' = extractLambdaDataflow lambda
-    in KnowledgeBase calls' args' lambdas' params' dataflow'
+    in KnowledgeBase calls' args' lambdas' params' dataflow' []
 
 extractFunctionDataflow :: Callable.FunctionContent -> [ Edge ]
 extractFunctionDataflow func = extractDataflow (Callable.funcBody func)
+
+extractMethodDataflow :: Callable.MethodContent -> [ Edge ]
+extractMethodDataflow method = extractDataflow (Callable.methodBody method)
 
 extractLambdaDataflow :: Callable.LambdaContent -> [ Edge ]
 extractLambdaDataflow lambda = extractDataflow (Callable.lambdaBody lambda)
@@ -174,6 +197,9 @@ extractLambdaParams lambda = extractParams (Callable.lambdaLocation lambda) (Cal
 
 extractFunctionParams :: Callable.FunctionContent -> [ Param ]
 extractFunctionParams f = extractParams (Token.getFuncNameLocation (Callable.funcName f)) (Callable.funcBody f)
+
+extractMethodParams :: Callable.MethodContent -> [ Param ]
+extractMethodParams m = extractParams (Token.getMethodNameLocation (Callable.methodName m)) (Callable.methodBody m)
 
 extractParams :: Location -> Cfg -> [ Param ]
 extractParams location cfg = let
