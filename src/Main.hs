f5fb1bc419db53f5511fcbaedfa3fed18d59101f
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-06-19 15:52:59 +0300

    increase maximal allowed request body size

--- src/Main.hs
+++ src/Main.hs
@@ -29,7 +29,7 @@ mkYesod "App" [parseRoutes|
 /healthcheck HealthcheckR GET
 |]
 
-instance Yesod App
+instance Yesod App where maximumContentLength = \app -> (\anyRouteReally -> Just 32000000)
 
 getHealthcheckR :: Handler Value
 getHealthcheckR = returnJson $ Healthy True
